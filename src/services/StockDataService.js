d44f7bc8cc92675991cbc5f15a66f3266b7fa73e
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-10-16 14:53:24 +0530

    Update StockDataService.js

--- src/services/StockDataService.js
+++ src/services/StockDataService.js
@@ -1,30 +1,6 @@
 import http from "../http-common";
 
 class StockDataService {
-  getAll() {
-    return http.get("/");
-  }
-
-  get(id) {
-    return http.get(`/${id}`);
-  }
-
-  create(data) {
-    return http.post("/", data);
-  }
-
-  // update(id, data) {
-  //   return http.put(`/tutorials/${id}`, data);
-  // }
-
-  // delete(id) {
-  //   return http.delete(`/tutorials/${id}`);
-  // }
-
-  deleteAll() {
-    return http.delete(`/`);
-  }
-
   findByTitle(title) {
     return http.get(`/search?keywords=${title}`);
   }
